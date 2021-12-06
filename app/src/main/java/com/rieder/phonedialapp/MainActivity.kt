5e3f10602f25f6403a2068064b9e2d7a91cbf7f5
rim36739 <maximilian.rieder@st.oth-regensburg.de>
2021-12-06 22:15:42 +0000

    finished prototype

--- app/src/main/java/com/rieder/phonedialapp/MainActivity.kt
+++ app/src/main/java/com/rieder/phonedialapp/MainActivity.kt
@@ -21,34 +21,78 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    //todo instanciate other 2
     private var phoneNumber = ""
     private var quickCall1Name = ""
     private var quickCall1Number = ""
+    private var quickCall2Name = ""
+    private var quickCall2Number = ""
+    private var quickCall3Name = ""
+    private var quickCall3Number = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //get data from db
         val sharedPreferences = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        //todo do for others 2
         quickCall1Name = sharedPreferences.getString("quickCall1Name", "").toString()
         quickCall1Number = sharedPreferences.getString("quickCall1Number", "").toString()
-        val textView: TextView = findViewById<TextView>(R.id.quick_text_1)
-        textView.text = quickCall1Name
+        val textView1: TextView = findViewById<TextView>(R.id.quick_text_1)
+        textView1.text = quickCall1Name + "\n" + quickCall1Number
+        quickCall2Name = sharedPreferences.getString("quickCall2Name", "").toString()
+        quickCall2Number = sharedPreferences.getString("quickCall2Number", "").toString()
+        val textView2: TextView = findViewById<TextView>(R.id.quick_text_2)
+        textView2.text = quickCall2Name + "\n" + quickCall2Number
+        quickCall3Name = sharedPreferences.getString("quickCall3Name", "").toString()
+        quickCall3Number = sharedPreferences.getString("quickCall3Number", "").toString()
+        val textView3: TextView = findViewById<TextView>(R.id.quick_text_3)
+        textView3.text = quickCall3Name + "\n" + quickCall3Number
 
+        //actions for buttons
         val quick_button_1 = findViewById<Button>(R.id.quick_button_1)
-        quick_button_1.setOnLongClickListener(object : View.OnLongClickListener {
-            override fun onLongClick(v: View?): Boolean {
-                showQuickDialog(1)
-                return true
-            }
-        })
-        quick_button_1.setOnClickListener {
-            Log.i("asd", "Long2")
+        quick_button_1.setOnLongClickListener {
+            showQuickDialog(1)
+            true
+        }
+        val quick_button_2 = findViewById<Button>(R.id.quick_button_2)
+        quick_button_2.setOnLongClickListener {
+            showQuickDialog(2)
+            true
+        }
+        val quick_button_3 = findViewById<Button>(R.id.quick_button_3)
+        quick_button_3.setOnLongClickListener {
+            showQuickDialog(3)
+            true
         }
     }
 
-    //todo write function for quick dial
+    fun quickCall(view: View) {
+        when (view.id) {
+            R.id.quick_button_1 -> {
+                Log.i("asd", "asnkfjkasjfjasfoi")
+                val intent = Intent(
+                    Intent.ACTION_DIAL,
+                    Uri.parse("tel:$quickCall1Number")
+                )
+                startActivity(intent)
+            }
+            R.id.quick_button_2 -> {
+                val intent = Intent(
+                    Intent.ACTION_DIAL,
+                    Uri.parse("tel:$quickCall2Number")
+                )
+                startActivity(intent)
+            }
+            R.id.quick_button_3 -> {
+                val intent = Intent(
+                    Intent.ACTION_DIAL,
+                    Uri.parse("tel:$quickCall3Number")
+                )
+                startActivity(intent)
+            }
+            else -> throw RuntimeException("Unknow button ID")
+        }
+    }
 
     fun showQuickDialog(forQuickSave :Int) {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
@@ -82,7 +126,6 @@ class MainActivity : AppCompatActivity() {
         builder.show()
     }
 
-    //todo do for the other 2
     fun setNameNumberForQuick(forIndex :Int, name :String, number :String) {
         val sharedPreferences = getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
         when(forIndex) {
@@ -90,7 +133,7 @@ class MainActivity : AppCompatActivity() {
                 quickCall1Name = name
                 quickCall1Number = number
                 val textView: TextView = findViewById<TextView>(R.id.quick_text_1)
-                textView.text = name
+                textView.text = name + "\n" + number
                 with (sharedPreferences.edit()) {
                     putString("quickCall1Name", name)
                     putString("quickCall1Number", number)
@@ -98,10 +141,26 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             2 -> {
-
+                quickCall2Name = name
+                quickCall2Number = number
+                val textView: TextView = findViewById<TextView>(R.id.quick_text_2)
+                textView.text = name + "\n" + number
+                with (sharedPreferences.edit()) {
+                    putString("quickCall2Name", name)
+                    putString("quickCall2Number", number)
+                    apply()
+                }
             }
             3 -> {
-
+                quickCall3Name = name
+                quickCall3Number = number
+                val textView: TextView = findViewById<TextView>(R.id.quick_text_3)
+                textView.text = name + "\n" + number
+                with (sharedPreferences.edit()) {
+                    putString("quickCall3Name", name)
+                    putString("quickCall3Number", number)
+                    apply()
+                }
             }
         }
 
